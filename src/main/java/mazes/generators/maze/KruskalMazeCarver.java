8aef5710a2b8baf1edee9309397a26606aee4566
mruby7188 <mruby7188@gmail.com>
2019-01-03 22:10:20 -0800

    fix internal structure and graph handling

--- src/main/java/mazes/generators/maze/KruskalMazeCarver.java
+++ src/main/java/mazes/generators/maze/KruskalMazeCarver.java
@@ -21,16 +21,18 @@ public class KruskalMazeCarver implements MazeCarver {
         //
         // In particular, if you call 'wall.setDistance()' at any point, make sure to
         // call 'wall.resetDistanceToOriginal()' on the same wall before returning.
-        Random rand = new Random();
+    	Random rand = new Random();
+        
+        ISet<Wall> walls = new ChainedHashSet<Wall>();
         
         for (Wall wall : maze.getWalls()) {
-            wall.setDistance(rand.nextDouble());
+            Wall temp = wall;
+            temp.setDistance(rand.nextDouble());
+            walls.add(temp);
         }
+        Graph<Room, Wall> graph = new Graph<Room, Wall>(maze.getRooms(), walls);
 
-        ISet<Wall> mst = new Graph<Room, Wall>(maze.getRooms(), maze.getWalls()).findMinimumSpanningTree();
-        for (Wall wall : maze.getWalls()) {
-        	wall.resetDistanceToOriginal();
-        }
-        return mst;
+        return graph.findMinimumSpanningTree();
     }
+
 }
--- src/main/java/misc/graphs/Graph.java
+++ src/main/java/misc/graphs/Graph.java
@@ -13,6 +13,7 @@ import datastructures.interfaces.IPriorityQueue;
 import datastructures.interfaces.ISet;
 import datastructures.concrete.ChainedHashSet;
 import misc.exceptions.NoPathExistsException;
+import misc.Searcher;
 
 /**
  * Represents an undirected, weighted graph, possibly containing self-loops, parallel edges,
@@ -73,7 +74,7 @@ public class Graph<V, E extends Edge<V> & Comparable<E>> {
      */
     public Graph(IList<V> vertices, IList<E> edges) {
         this.graph = new ChainedHashDictionary<V, IList<E>>();
-        this.edges = topKSort(edges.size(), edges);
+        this.edges = Searcher.topKSort(edges.size(), edges);
         this.vertices = new ArrayDisjointSet<V>();
         for (V vertex : vertices) {
             this.graph.put(vertex, new DoubleLinkedList<E>());
@@ -137,22 +138,15 @@ public class Graph<V, E extends Edge<V> & Comparable<E>> {
      */
     public ISet<E> findMinimumSpanningTree() {
         ISet<E> minEdges = new ChainedHashSet<E>();
-        IDisjointSet<V> vertexSet = vertices;
-        if (graph.size() == 1) {
-            return minEdges;
-        } else {
-            for (E edge : this.edges) {
-                V vertex = edge.getVertex1();
-                if (vertexSet.findSet(vertex) != vertexSet.findSet(edge.getOtherVertex(vertex))) {
-                    minEdges.add(edge);
-                    vertexSet.union(vertex, edge.getOtherVertex(vertex));
-                }
-                if (this.graph.size() - 1 == minEdges.size()) {
-                    return minEdges;
-                }
+        IDisjointSet<V> vertexSet = this.vertices;
+        for (E edge : this.edges) {
+            V vertex = edge.getVertex1();
+            if (vertexSet.findSet(vertex) != vertexSet.findSet(edge.getOtherVertex(vertex))) {
+                minEdges.add(edge);
+                vertexSet.union(vertex, edge.getOtherVertex(vertex));
             }
         }
-        throw new NoPathExistsException();
+        return minEdges;
     }
 
     /**
@@ -211,60 +205,6 @@ public class Graph<V, E extends Edge<V> & Comparable<E>> {
             }
         }        
         throw new NoPathExistsException();
-    }
-
-    /**
-     * This method takes the input list and returns the top k elements
-     * in sorted order.
-     *
-     * So, the first element in the output list should be the "smallest"
-     * element; the last element should be the "biggest".
-     *
-     * If the input list contains fewer then 'k' elements, return
-     * a list containing all input.length elements in sorted order.
-     *
-     * This method must not modify the input list.
-     *
-     * @throws IllegalArgumentException  if k < 0
-     */
-    private static <T extends Comparable<T>> IList<T> topKSort(int k, IList<T> input) {
-        // Implementation notes:
-        //
-        // - This static method is a _generic method_. A generic method is similar to
-        //   the generic methods we covered in class, except that the generic parameter
-        //   is used only within this method.
-        //
-        //   You can implement a generic method in basically the same way you implement
-        //   generic classes: just use the 'T' generic type as if it were a regular type.
-        //
-        // - You should implement this method by using your ArrayHeap for the sake of
-        //   efficiency.
-
-        if (k < 0) {
-            throw new IllegalArgumentException();
-        }
-        if (input.size() < k) {
-            k = input.size();
-        }
-        IPriorityQueue<T> heap = new ArrayHeap<T>();
-        IList<T> result = new DoubleLinkedList<T>();
-        int count = 0;
-        if (k == 0) {
-            return result;
-        }
-        for (T item : input) {
-            if (count < k) {
-                count++;
-                heap.insert(item);
-            } else if (item.compareTo(heap.peekMin()) > 0) {
-                heap.removeMin();
-                heap.insert(item);
-            }
-        }
-        while (!heap.isEmpty()) {
-            result.add(heap.removeMin());
-        }
-        return result;
     }
 
     private class EdgeDistance implements Comparable<EdgeDistance> {
